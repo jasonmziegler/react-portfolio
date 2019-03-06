a297e8861bde4016b9a6ab4eb60c30c905da5e95
jasonmziegler <jason@insightfulautomation.com>
2019-03-06 12:08:14 -0600

    replace create-react-app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Switch, Route, BrowserRouter } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
+import App from './components/App';
+import Jokes from './components/Jokes';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { create } from 'domain';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<BrowserRouter>
+    <Router history={createBrowserHistory()}>
+        <Switch>
+            <Route exact path='/' component={App} />
+            <Route path='/jokes' component={Jokes} />
+        </Switch>
+    </Router>
+</BrowserRouter>,
+ document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
